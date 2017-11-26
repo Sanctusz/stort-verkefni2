9a5d59bb6898458a75e5ccbccfdcc51f83286c0e
Sanctusz <yohans89@gmail.com>
2017-11-26 17:16:52 +0000

    HTML conflict

--- scripts.js
+++ scripts.js
@@ -17,13 +17,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
 var program = (function() {
 
+  function show(video) {
+    var div = document.createElement("div");
+    var divname = ["ID:", "Titill:", "Gert:", "Lengd:", "Mynd:", "Myndband:"];
+    var divcontent = [video.id, video.title, video.created, video.duration, video.poster, video.video];
+    for (var i = 0; i < divname.length; i++) {
+      var divn = document.createElement("div");
+      var divc = document.createElement("div");
+      divn.innerHTML = divname[i];
+      divc.innerHTML = divcontent[i];
+      console.log(divname);
+      div.appendChild(divn);
+      div.appendChild(divc);
+    }
+    result.appendChild(div);
+  }
+
   function init() {
     loadJSON(function(response) {
       // Parse JSON string into object
       var videodata = JSON.parse(response);
       var video = videodata.videos;
+      result = document.querySelector("div");
       console.log("LOADED");
-      console.log(video[0].title);
+      console.log("Video nr 1 er:" + video[0].title);
+      show(videodata.videos[0]);
     });
   }
 

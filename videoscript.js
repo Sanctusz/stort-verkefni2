b40e11760af4eb213f90d14dc31181aca9e24100
Nevatrue <nevatru@gmail.com>
2017-11-27 04:04:05 +0000

    more useless js
    held það þurfi að gera eitthvað onload function þannig við getum notað bara eitt javacsript file

--- videoscript.js
+++ videoscript.js
@@ -5,5 +5,29 @@ var getQueryString = function ( field, url ) {
     return string ? string[1] : null;
 };
 
-var thisOne = getQueryString('this');
+var thisOne = getQueryString(string);
 console.log(string);
+
+
+
+var buttonDiv = document.createElement("div");
+result
+
+function changePlayImage()
+{
+document.images["jsbutton"].src= "./img/play.svg";
+return true;
+}
+function changePauseImageBack()
+{
+ document.images["jsbutton"].src = "./img/pause.svg";
+ return true;
+}
+
+//
+var videodata = JSON.parse(response);
+var video = videodata.videos;
+var videoCatagories = videodata.categories;
+result = document.querySelector("div");
+result = document.querySelector("div");
+
